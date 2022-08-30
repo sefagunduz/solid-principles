f8af0d04b7ed86a4c354ce0fbc851471107259bb
Sefa GÜNDÜZ <sefagunduz@outlook.com.tr>
2022-08-30 23:10:39 +0300

    Open Closed Principle

---
+++ solid-principles/OpenClosed/Bad/Log.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles.OpenClosed.Bad
+{
+    public enum SavingType
+    {
+        xml, json, txt
+    }
+
+    public class Log
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public SavingType savingType { get; set; }
+
+        public void Add(Log log)
+        {
+            if (log.savingType == SavingType.xml)
+            {
+                // saving xml log
+            }
+            if (log.savingType == SavingType.json)
+            {
+                // saving json log
+            }
+            if (log.savingType == SavingType.txt)
+            {
+                // saving txt log
+            }
+        }
+    }
+}
---
+++ solid-principles/OpenClosed/Good/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles.OpenClosed.Good
+{
+    public abstract class Logger
+    {
+        public abstract void Add(Log log);
+    }
+
+    public class XmlLogger : Logger
+    {
+        public override void Add(Log log)
+        {
+            // saving xml log
+        }
+    }
+
+    public class JsonLogger : Logger
+    {
+        public override void Add(Log log)
+        {
+            // saving json log
+        }
+    }
+
+    public class TxtLogger : Logger
+    {
+        public override void Add(Log log)
+        {
+            // saving txt log
+        }
+    }
+
+
+}
